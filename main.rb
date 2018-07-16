ad6491412eac89457840818fd52cb0efb14bf391
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2018-07-16 09:22:07 -0300

    refactor gateway

--- main.rb
+++ main.rb
@@ -2,14 +2,7 @@ require 'google/apis/calendar_v3'
 require './boundaries/infra/authorization'
 require './boundaries/infra/gateway'
 
-APPLICATION_NAME = 'Google Calendar'.freeze
-
-service = Google::Apis::CalendarV3::CalendarService.new
-authorization = Boundaries::Infra::Authorization.new.authorize(service)
-service.client_options.application_name = APPLICATION_NAME
-service.authorization = authorization
-
-events = Boundaries::Infra::Gateway.new(service).list_events
+events = Boundaries::Infra::Gateway.new.list_events
 
 puts 'Upcoming events:'
 puts 'No upcoming events found' if events.empty?
